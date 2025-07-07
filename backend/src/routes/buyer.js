7d3ca5310622812eefe3eb63c7169fffbb912551
Cuong1312 <118240417+Cuong1312@users.noreply.github.com>
2025-07-07 18:10:02 +0700

    Update buyer.js

--- backend/src/routes/buyer.js
+++ backend/src/routes/buyer.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const BuyerController = require('../controllers/users');
 
-
+//
 router.post('/', BuyerController.register)
 router.post('/login', BuyerController.login)
 
