41c2ad0c5dd1d47dabe8f5d1cedf4638e91fced3
Cuong1312 <118240417+Cuong1312@users.noreply.github.com>
2025-07-07 18:05:36 +0700

    Update userSlice.js

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -18,7 +18,7 @@ export const userSlice = createSlice({
 });
 
 export const { login, logout } = userSlice.actions;
-
+//
 export const selectUser = (state) => {
   const stateTest = state.user.user;
   return JSON.parse(localStorage.getItem("user"));
